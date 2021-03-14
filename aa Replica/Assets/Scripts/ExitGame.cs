88efc3f51efeeec100c2655ed6c24ec4e8a0ce72
Nathan McGee <nathanmcgee85@yahoo.com>
2021-03-13 22:58:26 -0600

    Modifications

---
+++ aa Replica/Assets/Scripts/ExitGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitGame : MonoBehaviour
+{
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ aa Replica/Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject pinPrefab;
+
+    public static string playernamestr;
+
+    public Text playername;
+
+    void Start ()
+    {
+        playername.text = playernamestr;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnPin();
+        }
+    }
+
+    void SpawnPin ()
+    {
+        Instantiate(pinPrefab, transform.position, transform.rotation);
+    }
+}
